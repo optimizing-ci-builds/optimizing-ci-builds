2b84f06f9b27b31c4c1f241b1fbd4c6794aeadd0
optimizing-ci-builds <108610715+optimizing-ci-builds@users.noreply.github.com>
2022-09-20 22:13:08 +0300

    Update utils.py

--- job_analyzer/utils.py
+++ job_analyzer/utils.py
@@ -11,21 +11,29 @@ import pandas as pd
 import numpy as np
 
 base_api_url: str = "https://api.github.com"
-user_token: str = os.environ["G_AUTH_OP"]
+# user_token: str = os.environ["G_AUTH_OP"]
 headers: dict = {"Accept": "application/vnd.github+json",
                  "Authorization": f"token {user_token}"}
 
 
-def fork_project(owner: str, repo: str):
-    url = f"{base_api_url}/repos/optimizing-ci-builds/{repo}"
-    requests.delete(url=url, headers=headers)
-    # if "id" not in response:
-    url_path: str = f"{base_api_url}/repos/{owner}/{repo}/forks"
-    response = requests.post(url_path, headers=headers)
-    if response.status_code != 202:
-        raise ValueError(
-            f"There have been a problem while forking {owner}/{repo}. Error: {response.text}")
-    return response.json()
+def get_filtered_repos():
+    repositories = []
+    with open("data/filtered_repositories.csv", "r", newline="", encoding="utf8") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        next(csv_reader, None)
+        for row in csv_reader:
+            repositories.append(
+                {"name": row[0], "link": row[1], "default_branch": row[2], "sha": row[3],
+                "stargazers_count": row[4], "forks_count": row[5], "Date": row[6],
+                "Maven": row[7], "Gradle": row[8], "Travis CI": row[9], "Github Actions": row[10]})
+    return repositories
+
+
+def retrieve_sha(owner: str, repo: str, default_branch: str):
+    url = f"https://api.github.com/repos/{owner}/{repo}/branches/{default_branch}"
+    response = requests.get(url=url, headers=headers).json()
+    sha = response['commit']['sha']
+    return sha
 
 
 def get_yaml_file(forked_owner: str, repo: str, file_path: str):
@@ -119,9 +127,17 @@ def setup_runner(token, owner, repo):
         os.system(
             f"echo | ./config.sh --url https://github.com/{owner}/{repo} --token {token}")
     return token
-  
 
-def commit_file(owner: str, repo: str, sha : str, default_branch:str ,file_paths, new_file_contents, yaml_shas):
+
+def execute(owner: str, repo: str, sha:str, default_branch:str, file_paths, new_files, yaml_shas):
+    proc1 = subprocess.Popen(f"inotifywait -mr _work/ --format '%T;%w;%f;%e' --timefmt %T -o ../{repo}_logs/{owner}-{repo}.csv", shell=True)
+    proc2 = subprocess.Popen("./run.sh")
+    print("Processes created.")
+    commit_sha = commit_file(owner, repo, sha, default_branch,file_paths, new_files, yaml_shas)
+    return proc1, proc2, commit_sha
+
+
+def commit_file(owner: str, repo: str, sha : str, default_branch:str, file_paths, new_file_contents, yaml_shas):
     create_branch(owner, repo, sha)
     blob_shas = create_blobs(owner, repo, new_file_contents)
     tree_sha = create_tree(owner, repo, sha, file_paths, blob_shas)
@@ -132,12 +148,14 @@ def commit_file(owner: str, repo: str, sha : str, default_branch:str ,file_paths
 
 
 def create_branch(owner, repo, sha):
+    response = requests.delete(f"{base_api_url}/repos/{owner}/{repo}/git/refs/heads/optimizing-ci-build")
+    print(response)
     url = f"{base_api_url}/repos/{owner}/{repo}/git/refs"
     body = {
             "ref": "refs/heads/optimizing-ci-builds",
             "sha": sha
     }
-    response = requests.post(url=url, data=json.dumps(body), headers=headers)
+    requests.post(url=url, data=json.dumps(body), headers=headers)
 
 
 def create_blobs(owner, repo, new_file_contents):
@@ -199,32 +217,6 @@ def open_pull_request(owner: str, repo: str, default_branch:str ):
         "base": default_branch
     }
     response = requests.post(url=url, data=json.dumps(body), headers=headers)
-
-def execute(owner: str, repo: str, sha:str, default_branch:str, file_paths, new_files, yaml_shas):
-    proc1 = subprocess.Popen(f"inotifywait -mr _work/ --format '%T;%w;%f;%e' --timefmt %T -o ../{repo}_logs/{owner}-{repo}.csv", shell=True)
-    proc2 = subprocess.Popen("./run.sh")
-    print("Processes created.")
-    commit_sha = commit_file(owner, repo, sha, default_branch,file_paths, new_files, yaml_shas)
-    return proc1, proc2, commit_sha
-
-
-def get_filtered_repos():
-    repositories = []
-    with open("data/filtered_repositories.csv", "r", newline="", encoding="utf8") as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        next(csv_reader, None)
-        for row in csv_reader:
-            repositories.append(
-                {"name": row[0], "link": row[1], "default_branch": row[2], "sha": row[3],
-                "stargazers_count": row[4], "forks_count": row[5],
-                "Maven": row[6], "Gradle": row[7], "Travis CI": row[8], "Github Actions": row[9],
-                "MJacoco": row[10], "MCobertura": row[11], "MJavadoc": row[12],
-                "GJacoco": row[13], "GCobertura": row[14], "GJavadoc": row[15],
-                "Tyml_codecov": row[16], "Tyml_coveralls": row[17], "Tyml_codacy": row[18],
-                "Tyml_jacoco": row[19], "Tyml_cobertura": row[20], "Tyml_javadoc": row[21],
-                "Gyml_codecov": row[22], "Gyml_coveralls": row[23], "Gyml_codacy": row[24],
-                "Gyml_jacoco": row[25], "Gyml_cobertura": row[26], "Gyml_javadoc": row[27]})
-    return repositories
 
 
 def check_runs(owner: str, repo: str, commit_sha:str):
