a45655e8c2781167b7c38b317cc364714bda2488
Shanto Rahman <shanto@ECE-A55006.austin.utexas.edu>
2023-01-03 22:04:13 -0600

    script update to make cluster

--- result_parsing_And_cluster/make_cluster_for_each_category.sh
+++ result_parsing_And_cluster/make_cluster_for_each_category.sh
@@ -20,34 +20,17 @@ never_access=${uses_name}
 while read line 
 do
 	file_name=$(echo $line | cut -d',' -f2) 	
-	prefix_remove=$(sed 's;^.*target/;;g' <<< ${file_name})
-	echo ${prefix_remove} >> "cm_unsort_Prefix_remove.csv"
+    #check if a line contains a string
+    if [[ $file_name == *"target"* ]]; then
+        echo "**** ${file_name}"
+	    prefix_remove=$(sed 's;^.*target/;;g' <<< ${file_name})
+    	echo ${prefix_remove} >> "never_accessed_unsort_Prefix_remove.csv"
+    fi
 done < $1  #"contents_of_all_files_which_are_never_ever_accessed.csv"   
 
-sort "cm_unsort_Prefix_remove.csv" > "Clustering/${uses_name}_sort_Prefix_remove.csv"
-rm  "cm_unsort_Prefix_remove.csv"
+sort "never_accessed_unsort_Prefix_remove.csv" > "Clustering/${never_access}_sort_Prefix_remove.csv"
+rm  "never_accessed_unsort_Prefix_remove.csv"
 
-#==========For Making Cluster============
-group_arr=$(cut -f1 -d'/' "Clustering/${uses_name}_sort_Prefix_remove.csv"  | sort | uniq)
-echo "group,count" > "Clustering/${uses_name}_Clustering.csv"
-for group in ${group_arr[@]}; do
-    count=$(grep -r "$group" "Clustering/${uses_name}_sort_Prefix_remove.csv" | wc -l)
-    echo "$group,$count" >> "Clustering/${uses_name}_Clustering.csv"
-    if [[ $group == "site" ]]; then
-        grep -r "site" "Clustering/${uses_name}_sort_Prefix_remove.csv"  > "tmp_site.csv"
-         group_site_arr=$(cut -f2 -d'/' "tmp_site.csv"  | sort | uniq)
-         for group_site in ${group_site_arr[@]}; do
-            count_site=$(grep -r "${group_site}" "tmp_site.csv" | wc -l)
-            #echo ${group_site},${count_site}
-            echo "site/${group_site},${count_site}" >> "Clustering/${uses_name}_Clustering.csv"
-         done
-    fi
-done
-rm "Clustering/${uses_name}_sort_Prefix_remove.csv"
-
-if [[ -f "tmp_site.csv" ]]; then
-    rm "tmp_site.csv"
-fi
 #======================================== useful.csv ($3)===============================
 
 uses_name=$(echo $3 | rev | cut -d'/' -f1 | rev)
@@ -55,84 +38,47 @@ useful=${uses_name}
 
 while read line 
 do
-	file_name=$(echo $line | cut -d',' -f2)	
-	prefix_remove=$(sed 's;^.*target/;;g' <<< ${file_name})
-	echo ${prefix_remove} >> "useful_unsort_Prefix_remove.csv"
+    if [[ $file_name == *"target"* ]]; then
+    	file_name=$(echo $line | cut -d',' -f2)	
+	    prefix_remove=$(sed 's;^.*target/;;g' <<< ${file_name})
+    	echo ${prefix_remove} >> "useful_unsort_Prefix_remove.csv"
+    fi
 done < $3  #"contents_of_all_files_which_are_accessed.csv"   
-
-sort "useful_unsort_Prefix_remove.csv" > "Clustering/${uses_name}_useful_sort_Prefix_remove.csv"
+sort "useful_unsort_Prefix_remove.csv" > "Clustering/${useful}_useful_sort_Prefix_remove.csv"
 rm  "useful_unsort_Prefix_remove.csv"
 
-
-#==========For Making Cluster============
-group_arr=$(cut -f1 -d'/' "Clustering/${uses_name}_useful_sort_Prefix_remove.csv"  | sort | uniq)
-echo "group,count" > "Clustering/${uses_name}_Clustering.csv"
-for group in ${group_arr[@]}; do
-    count=$(grep -r "$group" "Clustering/${uses_name}_useful_sort_Prefix_remove.csv" | wc -l)
-    echo "$group,$count" >> "Clustering/${uses_name}_Clustering.csv"
-    if [[ $group == "site" ]]; then
-        grep -r "site" "Clustering/${uses_name}_useful_sort_Prefix_remove.csv"  > "tmp_site.csv"
-         group_site_arr=$(cut -f2 -d'/' "tmp_site.csv"  | sort | uniq)
-         for group_site in ${group_site_arr[@]}; do
-            count_site=$(grep -r "${group_site}" "tmp_site.csv" | wc -l)
-            #echo ${group_site},${count_site}
-            echo "site/${group_site},${count_site}" >> "Clustering/${uses_name}_Clustering.csv"
-         done
-    fi
-done
-rm "Clustering/${uses_name}_useful_sort_Prefix_remove.csv"
-
-if [[ -f "tmp_site.csv" ]]; then
-    rm "tmp_site.csv"
+#================= FOR COMPARING this two sorted csv =========================
+allClusters=()
+if [[ -f  "Clustering/$4-unnecessary.csv" ]]; then
+    rm "Clustering/$4-unnecessary.csv"
 fi
-
-#==========================WHICH ARE uncommon in useful file ==================================
-
-cat "Clustering/${never_access}_Clustering.csv" | cut -d',' -f1 > "tmp1_never"
-cat "Clustering/${useful}_Clustering.csv" | cut -d',' -f1 > "tmp2_useful"
-comm -13 <(sort -u "tmp2_useful") <(sort -u "tmp1_never") > "Clustering/$4-unnecessary.csv"  # Looking at is not in useful category but exists in never access
-rm "tmp1_never"
-rm "tmp2_useful"
-#================ WHICH are Aceesessed($2) ============================================
-    if [[ $2 == "" ]]; then
-        echo "arg2 is required"
-        exit
-    else
-    
-    uses_name=$(echo $2 | rev | cut -d'/' -f1 | rev)
-#---------Extract file names which have target directory ------------
-    while read line 
+while read line
+do
+    count_directory_structure=$(echo $line | tr -cd / | wc -c)
+    path=""
+    #for i in {1..$count_directory_structure}
+    for (( i=1; i<=$count_directory_structure; i++ ))
     do
-    	file_name=$(echo $line | cut -d',' -f2)	
-    	prefix_remove=$(sed 's;^.*target/;;g' <<< ${file_name})
-    	echo ${prefix_remove} >> "cma_unsort_Prefix_remove.csv"
-    done < $2  #"contents_of_all_files_which_are_accessed.csv"   
-    
-    if [[ -f "cma_unsort_Prefix_remove.csv" ]]; then
-        sort "cma_unsort_Prefix_remove.csv" > "${uses_name}_cma_sort_Prefix_remove.csv"
-        rm  "cma_unsort_Prefix_remove.csv"
-
-        #==========For Making Cluster============
-            
-        group_arr=$(cut -f1 -d'/' "${uses_name}_cma_sort_Prefix_remove.csv"  | sort | uniq)
-        echo "group,count" > "${uses_name}_cma_Clustering.csv"
-        for group in ${group_arr[@]}; do
-            count=$(grep -r "$group" "${uses_name}_cma_sort_Prefix_remove.csv" | wc -l)
-            echo "$group,$count" >> "${uses_name}_cma_Clustering.csv"
-            if [[ $group == "site" ]]; then
-                grep -r "site" "${uses_name}_cma_sort_Prefix_remove.csv"  > "tmp_site.csv"
-                 group_site_arr=$(cut -f2 -d'/' "tmp_site.csv"  | sort | uniq)
-                 for group_site in ${group_site_arr[@]}; do
-                    count_site=$(grep -r "${group_site}" "tmp_site.csv" | wc -l)
-                    #echo ${group_site},${count_site}
-                    echo "site/${group_site},${count_site}" >> "${uses_name}_cma_Clustering.csv"
-                 done
+        echo "i=$i"
+        dir=$(echo $line | cut -d'/' -f1-$i)
+        echo "dir=$dir"
+        if [[ ! " ${allClusters[*]} " =~ " ${dir} " ]]; then
+            found=$(grep -r "$dir" "Clustering/${useful}_useful_sort_Prefix_remove.csv" | wc -l)
+            echo $found
+            path=$dir"/"
+            if [[ $found -eq 0 ]]; then
+                echo $path >> "Clustering/$4-unnecessary-with-repetition.csv"
+                allClusters+=($path)
+                break;
             fi
-        done
-        
-        rm "tmp_site.csv"
-    fi
-fi
+        else
+            echo "need to count same cluster name"
+        fi
 
+    done
+done <  "Clustering/${never_access}_sort_Prefix_remove.csv"
+sort "Clustering/$4-unnecessary-with-repetition.csv" | uniq -c > "Clustering/$4-unnecessary.csv"
 
-#python3 pychart_generator.py "${uses_name}_cm_Clustering.csv"
+rm "Clustering/${useful}_useful_sort_Prefix_remove.csv"
+rm "Clustering/${never_access}_sort_Prefix_remove.csv"
+rm "Clustering/$4-unnecessary-with-repetition.csv"
