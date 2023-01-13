40ea2e343934abac3fc3eabdeaf7ebf21cc7cbdc
Shanto Rahman <shanto@ECE-A55006.austin.utexas.edu>
2023-01-13 11:09:35 -0600

    script update to read a csv

--- result_parsing_And_cluster/find_which_files_are_accessed_and_which_are_not.sh
+++ result_parsing_And_cluster/find_which_files_are_accessed_and_which_are_not.sh
@@ -7,95 +7,104 @@
 #mooc-software-testing.1672101501
 #git checkout -f  MavenTestCI.1670985148
 
-if [[ $1 == "" || $2 == "" ]]; then
-    echo "give $1 (directory name)"
-    echo "give $2 (Project name)"
+if [[ $1 == "" ]]; then
+    echo "give csv $1 (workflow_dir.csv)"
+    #echo "give $2 (Project name)"
 #    echo "give $3 (useful.csv)"
     exit
 fi
 currentDir=$(pwd)
-if [ -f "$currentDir/Output/$2-never-accessed" ]; then
-    rm "$currentDir/Output/$2-never-accessed"
-fi
-
-if [ -f "$currentDir/Output/$2-accessed" ]; then
-    rm "$currentDir/Output/$2-accessed"
-fi
-dir_arr=($(cd "$1/step-details/" && printf -- '%s\n' */))
-#$(find . -maxdepth 1 -type d -printf '%f\n')
-cd "$1/step-details/"
-#echo "PWD= ${dir_arr}"
-never_accessed_file_name_array=("cm_a.csv" "c_m_a.csv" "c_m__a.csv" "cm__a.csv"  "_cm_a.csv"  "_cm__a.csv.csv"  "_c_m_a.csv" "_c_m__a.csv" )
-accessed_file_name_array=("cma.csv" "c_ma.csv" "_cma.csv"  "_c_ma.csv"  )
-
-if [[ ! -d "$currentDir/Output" ]]; then
-    mkdir "$currentDir/Output"
-fi
-
-for i in "${dir_arr[@]}"
+Output="Output-1"
+while read row_line
 do
+    output_proj_name=$(echo $row_line |cut -d'/' -f8)
+    workflow_name=$(echo $row_line | rev |cut -d'/' -f1-2| rev | sed 's/\//-/g' )
+    proj_with_workflow="${output_proj_name}-$workflow_name"
 
-    echo "==========$i ========== $(pwd)"
-    if [[ "$i" =~ .*"checkout".* ]]; then
-        echo "checkout found"
-        continue
-    elif [[ "$i" =~ .*"setup".* ]]; then
-       echo "setup found"
-       continue
-    else
-        for j in "${never_accessed_file_name_array[@]}"
-        do
-            #echo $i$j
-            if [ -f $i$j ]; then
-                echo "Found $i$j"
-                cat "$i$j" >> "$currentDir/Output/$2-never-accessed"
-            else 
-                echo "Not Found"
-            fi
-        done
-
-        for k in "${accessed_file_name_array[@]}"
-        do
-            if [ -f $i$k ]; then
-                echo "pwd =$(pwd)"
-                echo $i$k
-                cat "$i$k" >> "$currentDir/Output/$2-accessed"
-            fi
-        done
-
+    if [ -f "$currentDir/$Output/$proj_with_workflow-never-accessed" ]; then
+        rm "$currentDir/$Output/$proj_with_workflow-never-accessed"
     fi
-    #if [[ $i != "." ]]; then
-    #fi
-done
-
-cat "$currentDir/Output/$2-never-accessed" | cut -d',' -f2 > "$currentDir/tmp1"
-cat "$currentDir/tmp1" | sort | uniq > "$currentDir/tmp"
-cp "$currentDir/tmp" "$currentDir/Output/$2-never-accessed" 
-rm "$currentDir/tmp1"
-rm "$currentDir/tmp"
-
-if [[ -f "$currentDir/Output/$2-accessed" ]]; then
-    cat "$currentDir/Output/$2-accessed" | cut -d',' -f2 > "$currentDir/tmp-access"
-    cat "$currentDir/tmp-access" | sort | uniq > "$currentDir/tmp-access1"
-    cp "$currentDir/tmp-access1" "$currentDir/Output/$2-accessed" 
-
-    rm "$currentDir/tmp-access1"
-    rm "$currentDir/tmp-access"
-
-    comm -13 <(sort -u "$currentDir/Output/$2-never-accessed") <(sort -u  "$currentDir/Output/$2-never-accessed") >  "$currentDir/Output/$2-common"
-fi
-
-### Process useful.csv
-cd $currentDir
-if [[ -f  "$currentDir/Output/$2-useful" ]]; then  
-    rm "$currentDir/Output/$2-useful"
-fi
-row_count=1
-while read line
-do
-    if [[ ${row_count} -gt 1 ]]; then
-        file_name=$(echo $line | cut -d',' -f2)
-        echo $file_name >>  "$currentDir/Output/$2-useful" 
+    
+    if [ -f "$currentDir/$Output/$proj_with_workflow-accessed" ]; then
+        rm "$currentDir/$Output/$proj_with_workflow-accessed"
     fi
-    row_count=$((row_count+1))
-done < "$1/useful.csv"
+    dir_arr=($(cd "$row_line/step-details/" && printf -- '%s\n' */))
+    #$(find . -maxdepth 1 -type d -printf '%f\n')
+    cd "$row_line/step-details/"
+    #echo "PWD= ${dir_arr}"
+    never_accessed_file_name_array=("cm_a.csv" "c_m_a.csv" "c_m__a.csv" "cm__a.csv"  "_cm_a.csv"  "_cm__a.csv.csv"  "_c_m_a.csv" "_c_m__a.csv" )
+    accessed_file_name_array=("cma.csv" "c_ma.csv" "_cma.csv"  "_c_ma.csv"  )
+    
+    if [[ ! -d "$currentDir/$Output" ]]; then
+        mkdir "$currentDir/$Output"
+    fi
+    
+    for i in "${dir_arr[@]}"
+    do
+    
+        echo "==========$i ========== $(pwd)"
+        if [[ "$i" =~ .*"checkout".* ]]; then
+            echo "checkout found"
+            continue
+        elif [[ "$i" =~ .*"setup".* ]]; then
+           echo "setup found"
+           continue
+        else
+            for j in "${never_accessed_file_name_array[@]}"
+            do
+                #echo $i$j
+                if [ -f $i$j ]; then
+                    echo "Found $i$j"
+                    cat "$i$j" >> "$currentDir/$Output/$proj_with_workflow-never-accessed"
+                else 
+                    echo "Not Found"
+                fi
+            done
+    
+            for k in "${accessed_file_name_array[@]}"
+            do
+                if [ -f $i$k ]; then
+                    echo "pwd =$(pwd)"
+                    echo $i$k
+                    cat "$i$k" >> "$currentDir/$Output/$proj_with_workflow-accessed"
+                fi
+            done
+    
+        fi
+        #if [[ $i != "." ]]; then
+        #fi
+    done
+    
+    cat "$currentDir/$Output/$proj_with_workflow-never-accessed" | cut -d',' -f2 > "$currentDir/tmp1"
+    cat "$currentDir/tmp1" | sort | uniq > "$currentDir/tmp"
+    cp "$currentDir/tmp" "$currentDir/$Output/$proj_with_workflow-never-accessed" 
+    rm "$currentDir/tmp1"
+    rm "$currentDir/tmp"
+    
+    if [[ -f "$currentDir/$Output/$proj_with_workflow-accessed" ]]; then
+        cat "$currentDir/$Output/$proj_with_workflow-accessed" | cut -d',' -f2 > "$currentDir/tmp-access"
+        cat "$currentDir/tmp-access" | sort | uniq > "$currentDir/tmp-access1"
+        cp "$currentDir/tmp-access1" "$currentDir/$Output/$proj_with_workflow-accessed" 
+    
+        rm "$currentDir/tmp-access1"
+        rm "$currentDir/tmp-access"
+    
+        comm -13 <(sort -u "$currentDir/$Output/$proj_with_workflow-never-accessed") <(sort -u  "$currentDir/$Output/$proj_with_workflow-never-accessed") >  "$currentDir/$Output/$proj_with_workflow-common"
+    fi
+    
+    ### Process useful.csv
+    cd $currentDir
+    if [[ -f  "$currentDir/$Output/$proj_with_workflow-useful" ]]; then  
+        rm "$currentDir/$Output/$proj_with_workflow-useful"
+    fi
+    row_count=1
+    while read line
+    do
+        if [[ ${row_count} -gt 1 ]]; then
+            file_name=$(echo $line | cut -d',' -f2)
+            echo $file_name >>  "$currentDir/$Output/$proj_with_workflow-useful" 
+        fi
+        row_count=$((row_count+1))
+    done < "$row_line/useful.csv"
+
+done < $1
