ffd2e714b2accdbc0108e940dda355285b3debb4
Shanto Rahman <shanto@ECE-A55006.austin.utexas.edu>
2023-03-05 21:36:41 -0600

    script bug fix

--- result_parsing_And_cluster/csv_generate_2.sh
+++ result_parsing_And_cluster/csv_generate_2.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
-#for item in ${arr_unnecessary_files_items[@]}A
+#for item in ${arr_unnecessary_files_items[@]}
 currentDir=$(pwd)
 cd $1
 while read line
 do
     #echo $item
     files=($(grep -lr "${line}$"))
-    #echo $m
-    #echo ${#m[@]}
+    total=0
     for file in ${files[@]}
     do
-        #echo $file
+        echo $file
         count=0
         proj_name=$(echo $file | cut -d':' -f1 | cut -d'#' -f1)
         job_name=$(echo $file | cut -d':' -f1 | cut -d'#' -f2 | sed 's/.csv//g')
-        res=($(grep -r "${line}" $file | cut -d'/' -f1 | sed 's/ //g'))
-        echo "len=${#res[@]}"
+        #echo $(cat file)
+        res=($(cat $file | cut -d'/' -f7- | grep -x -n "\b${line}$")) # |)) #13:lib/
+        echo $res
+        #echo "len=${#res[@]}"
         for i in ${res[@]}
         do
-           count=$((count + i)) 
+            line_num=$(echo $i | cut -d':' -f1)
+            echo $line_num
+            #exit
+            ll=$(head -${line_num} $file | tail -1)
+            echo $ll
+            l=$(echo $ll | cut -d'/' -f1 | sed 's/ //g')
+            echo $l
+            #exit
+            count=$(($count + $l)) 
+            total=$(($total+$l))
         done
-        echo "$line,$proj_name,$job_name,$count" >> "$currentDir/Histogram_for_each_unnecessary_file.csv"
+        if [[ $count -gt 0 ]]; then
+            echo "$line,$proj_name,$job_name,$count" >> "$currentDir/tmp.csv"
+        fi
     done
-    #exit
+    echo "total=$total"
+    exit
 done < "../x.csv"
+#sort -k1 "$currentDir/Histogram_for_each_unnecessary_file.csv"
+sort -k4 -n -t, -r "$currentDir/tmp.csv" > "$currentDir/Histogram_for_each_unnecessary_file.csv"
+rm "$currentDir/tmp.csv"
