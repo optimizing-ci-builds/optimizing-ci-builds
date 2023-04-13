798f4badc33310174b6f656f8b8aa58ecf2a9a66
Shanto Rahman <shanto@ECE-A55006.austin.utexas.edu>
2023-04-13 08:30:08 -0500

    adding another script

---
+++ result_parsing_And_cluster/find-immediate-one-dir-after-target.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [[ $1 == "" || $2 == "" ]]; then
+    echo "provide args (Clustering_Unused_dir , Result-File)"
+    exit
+fi
+
+find $1 -maxdepth 1 -size +0 -print > all_files.csv
+while read line 
+do
+    echo "$line============>" >> $2
+    arr=($(rev $line | cut -d'/' -f1-3 | rev | sort -u | grep  "^target/"))
+    for i in "${arr[@]}"
+    do
+      echo $i >> $2
+       # do whatever on "$i" here
+    done
+    echo "===========================" >> $2
+done < "all_files.csv"
+#(ls $1) >
