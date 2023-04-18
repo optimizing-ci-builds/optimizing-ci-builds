32623a2462ab903838e4b435ca855c1844bf71bb
Shanto Rahman <shanto@ECE-A55006.austin.utexas.edu>
2023-04-18 16:05:41 -0500

    script update

--- result_parsing_And_cluster/runAll.sh
+++ result_parsing_And_cluster/runAll.sh
@@ -9,14 +9,14 @@ currentDir=$(pwd)
 #exit
 #branch_name=$(echo $1 | rev | cut -d'/' -f1 | rev | cut -d'-' -f2- | cut -d'.' -f1)
 echo $branch_name
-if [[ ! -d "ci-analyzes" ]]; then
+if [[ ! -d "ccc/ci-analyzes" ]]; then
     git clone https://github.com/UT-SE-Research/ci-analyzes.git 
 fi
 
 while read line
 do 
 
-    cd "$currentDir/ci-analyzes"
+    cd "$currentDir/ccc/ci-analyzes"
     if [[ ${line} =~ ^\# ]]; then
         echo "Line starts with Hash $line"
         continue
@@ -26,12 +26,14 @@ do
     git checkout ${branch_name}
     cd $currentDir
     proj_name=$(echo $line | cut -d',' -f1) 
-    #workflow_path=$(echo $line | cut -d',' -f3) 
-    echo ${proj_name} 
-        workflow_job_name=$(echo ${line} | cut -d',' -f10 | cut -d'/' -f11- | sed 's;\/;-;g') # example, Parsing(https://github.com/UT-SE-Research/ci-analyzes/tree/1680156014-f3221fe/soot/.github/workflows/ci/BuildAndTest)
-        echo "workflow= $workflow_job_name"
-        echo "${proj_name}_${workflow_job_name}"
-        bash make_cluster_for_each_category.sh "${proj_name}_${workflow_job_name}" "Clustering-Unused-Directories" "Unused" "Useful"  # file_name in the inotofy dir ($1_$3), useful file_name in inotify dir ($1_$4), Loop through($1_$3) 
-        bash make_cluster_for_each_category.sh "${proj_name}_${workflow_job_name}" "Clustering-Used-Directories" "Useful"  "Unused" 
+    echo -n "$proj_name"  >> "$currentDir/clusters_info.csv"
+    workflow_path=$(echo ${line} | cut -d',' -f3)
+    echo -n ",${workflow_path}" >> "$currentDir/clusters_info.csv"
 
+    workflow_job_name=$(echo ${line} | cut -d',' -f10 | cut -d'/' -f11- | sed 's;\/;-;g') # example, Parsing(https://github.com/UT-SE-Research/ci-analyzes/tree/1680156014-f3221fe/soot/.github/workflows/ci/BuildAndTest)
+    echo ",${proj_name}_${workflow_job_name}.csv" >> "$currentDir/clusters_info.csv"
+
+    echo "${proj_name}_${workflow_job_name}"
+    bash make_cluster_for_each_category.sh "${proj_name}_${workflow_job_name}" "Clustering-Unused-Directories" "Unused" "Useful"  # file_name in the inotofy dir ($1_$3), useful file_name in inotify dir ($1_$4), Loop through($1_$3) 
+    bash make_cluster_for_each_category.sh "${proj_name}_${workflow_job_name}" "Clustering-Used-Directories" "Useful"  "Unused" 
 done < $1
