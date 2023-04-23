d427674ac312fb94c7137a1516ab2a7fde9c6da7
Shanto Rahman <shanto@ECE-A55006.austin.utexas.edu>
2023-04-23 12:51:11 -0500

    code clean

--- automation_in_finding_plugin/dynamic-approach/runAll.sh
+++ automation_in_finding_plugin/dynamic-approach/runAll.sh
@@ -34,90 +34,59 @@ do
             echo $JAVA_HOME
         fi
         
-        range_build_plugins=($(awk '/<build>/,/<\/build>/ {if(/<plugins>/) {start=NR}; if(/<\/plugins>/) {print start; print NR; exit}}' pom.xml))
-        echo ${range_build_plugins[@]}
-        Start_range="${range_build_plugins[0]}"
-        echo $"START range=$Start_range"
-        end_range="${range_build_plugins[1]}"
-        #build_line=$(grep -n '<build>' pom.xml | cut -d: -f1 | head -1)
-        sed -n "$Start_range,${end_range}p" pom.xml | awk -v adj=$Start_range '{printf("%-5d%s\n", NR-1+adj, $0)}' > tmp.xml
-        plugin_starting_loc=($(grep -n "<plugin>" "tmp.xml" | cut -d':' -f2 | cut -d' ' -f1)) #becayse after greping we get 2:79 <plugin> 98:175 <plugin>". So, we need to extract 79 because this is the original line in the pom.xml
-        echo "starting loc=${plugin_starting_loc[@]}"
-        echo $(pwd)
         cp "pom.xml" "pom_org.xml"
-        for plugin_start in ${plugin_starting_loc[@]}
+        tildeCount=$(echo ${unused_dirs} | tr -cd '~' | wc -c) #multiple unused directory might exists
+        echo ${unused_dirs} ${tildeCount}
+        for (( i=1; i<=${tildeCount}; i++))
         do
-            how_far_plugin_end_from_this_given_line=$(sed -n "$plugin_start,\$p" pom.xml | grep -n "</plugin>" | head -1 | cut -d':' -f1)
-            plugin_end=$((how_far_plugin_end_from_this_given_line + plugin_start -1))
-            #sed -i "${plugin_start},${plugin_end}s/^/<!-- /; ${plugin_start},${plugin_end}s/$/ -->/" pom.xml
-            sed -i "${plugin_start},${plugin_end} {
-            /^\s*<!--/b   # skip lines that are already commented
-            s/^/<!-- /    # add comment tag to beginning of line
-            s/$/ -->/     # add comment tag to end of line
-            }" pom.xml
-            echo "start=$plugin_start, end=$plugin_end, pppppppppp"
-            #now run the mvn command  
-            #check if the unused directory exists or not (find -name ..), if no directory found. we will report the plugin name
-            if [ -n "$(find "target/" -name $unused-dir)" ]; then 
-                echo "found"
-                cp "pom_org.xml" "pom.xml"
-            else
-                echo "no-found"
-                #returning the plugin name
+            unnecessary_dir=$(echo "$unused_dirs" | cut -d'~' -f$i)
+            semicolon_found_indicates_file=$(echo  $unnecessary_dir | grep ";" | wc -l)
+            if [[ $semicolon_found_indicates_file -eq 0 ]]; then
+                range_build_plugins=($(awk '/<build>/,/<\/build>/ {if(/<plugins>/) {start=NR}; if(/<\/plugins>/) {print start; print NR; exit}}' pom.xml))
+                Start_range="${range_build_plugins[0]}"
+                end_range="${range_build_plugins[1]}"
+                sed -n "$Start_range,${end_range}p" pom.xml | awk -v adj=$Start_range '{printf("%-5d%s\n", NR-1+adj, $0)}' > tmp.xml
+                plugin_starting_loc=($(grep -n "<plugin>" "tmp.xml" | cut -d':' -f2 | cut -d' ' -f1)) #becayse after greping we get 2:79 <plugin> 98:175 <plugin>". So, we need to extract 79 because this is the original line in the pom.xml
+                echo "starting loc=${plugin_starting_loc[@]}"
+                for plugin_start in ${plugin_starting_loc[@]}
+                do
+                    how_far_plugin_end_from_this_given_line=$(sed -n "$plugin_start,\$p" pom.xml | grep -n "</plugin>" | head -1 | cut -d':' -f1)
+                    plugin_end=$((how_far_plugin_end_from_this_given_line + plugin_start -1))
+                    sed -i "${plugin_start},${plugin_end} {
+                    /^\s*<!--/b   # skip lines that are already commented
+                    s/^/<!-- /    # add comment tag to beginning of line
+                    s/$/ -->/     # add comment tag to end of line
+                    }" pom.xml
+                    echo "start=$plugin_start, end=$plugin_end, $workflow_file, $java_version"
+                    #now run the mvn command  
+                    last_level_dir=$(echo $unnecessary_dir | rev | cut -d'/' -f2 | rev)
+                    mvn clean
+                    ${mvn_command} > "log_${last_level_dir}_${plugin_start}.txt"
+                    #echo $last_level_dir
+                    #check if the unused directory exists or not (find -name ..), if no directory found. we will report the plugin name
+                    if [ -n "$(find "target/" -name $last_level_dir)" ]; then 
+                        echo "found"
+                        cp "pom_org.xml" "pom.xml"
+                    else
+                        echo "not-found"
+                        echo $Start_range
+                        echo $end_range
+                        groupId_index=$((Start_range + 1))
+                        artifactId_index=$((Start_range + 1))
+                        groupId=$(sed -n "${groupId_index}{s/.*>\(.*\)<.*/\1/p;q;}" pom_org.xml)
+                        echo $groupId
+                        artifactId=$(sed -n "${artifact_index}{s/.*>\(.*\)<.*/\1/p;q;}" pom_org.xml)
+                        echo $artifactId
+                        #find the plugin name
+                        echo "$unnecessary_dir,$groupId#$artifactId" >> "$currentDir/Result.csv"
+                        cp "pom_org.xml" "pom.xml"
+                        break
+                    fi
+                done
+                #exit
             fi
-            #exit
         done
-
-        
-        #$mvn_command  > "$currentDir/log" #whole command that they originally used
-        #sed -n '/<plugins>/,/<\/plugins>/p' pom.xml > "tmp.xml"
-        #sed -n "1,${profile_start}p; ${profile_end},\$p; /<plugins>/,/<\/plugins>/p" pom.xml > "tmp.xml"
-        #sed -n "1,${profile_start}p; ${profile_end},\$p; /<profiles>/,/<\/profiles>/b; /<plugins>/,/<\/plugins>/p" pom.xml > "tmp.xml"
-        #sed "${profile_start},${profile_end}c\\" pom.xml > "tmp.xml"
-        #sed '/<profiles>/,/<\/profiles>/d' pom.xml > "tmp.xml"
-        
-
-
-        exit
-        input=$(grep -n "<plugin>" tmp.xml)
-        #input="2: <plugin> 98: <plugin> 152: <plugin> 175: <plugin> 214: <plugin> 254: <plugin> 267: <plugin> 296: <plugin> 325: <plugin> 341: <plugin> 346: <plugin> 356: <plugin> 367: <plugin> 388: <plugin> 400: <plugin> 412: <plugin>"
-        #IFS=' ' read -ra array <<< "$(echo "$input" | cut -d ':' -f1)"
-        #IFS=' ' read -ra array <<< "$(echo "$input" | cut -f1 -d' ')"
-        #echo "${array[@]}"
-        exit
-        IFS=' ' read -ra arr <<< "$(echo "$input" | cut -d ':' -f1)"
-        echo ${arr[@]}
-        for plugin in "${arr[@]}"; do
-          #echo "$plugin"
-          line=$(echo $plugin | cut -d':' -f1)
-          echo "LLL=$line"
-          sed -n "$line,+2p" tmp.xml
-          echo "HA HA"
-          exit
-        done
-        exit
-        if [[ -f effective-pom.xml ]]; then
-            cd $currentDir
-            #Find each unused dir one by one
-            tildeCount=$(echo ${unused_dirs} | tr -cd '~' | wc -c)
-            echo ${unused_dirs} ${tildeCount}
-            for (( i=1; i<=${tildeCount}; i++))
-            do
-                unnecessary_dir=$(echo "$unused_dirs" | cut -d'~' -f$i)
-                semicolon_found_indicates_file=$(echo  $unnecessary_dir | grep ";" | wc -l)
-                #echo "Should be greater than 1=$semicolon_found_indicates_file"
-
-                if [[ $semicolon_found_indicates_file -gt -1 ]]; then
-                    #echo "UNU $unnecessary_dir"
-                    echo -n "../projects/$proj_name/effective-pom.xml,${unnecessary_dir}," >> "$currentDir/Result.csv"
-                    python3 find_plugin_corpus.py "../projects/$proj_name/effective-pom.xml" ${unnecessary_dir}
-                    #echo "SHANTO*** ${unnecessary_dir}"
-                    echo "" >> "$currentDir/Result.csv"
-                    #exit
-                fi
-            done
-        fi
-        #exit
+        #exit 
     fi
     header=false
 done < $1
